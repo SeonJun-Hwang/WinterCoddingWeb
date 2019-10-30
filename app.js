b0b2fdfcb159c9d11b3abc895a9a458951b4e87f
Girlfirend-Yerin <hsunj2003@naver.com>
2019-10-30 12:40:27 +0900

    Fix: Change Layout Template
    제공해주는 Template 적용
    javscript static path 추가
    stylesheets 일부 변경
    index, layout 추가 변경

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// set Static Route
+app.use('/js', express.static(path.join(__dirname, '/public/javascripts')))
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
