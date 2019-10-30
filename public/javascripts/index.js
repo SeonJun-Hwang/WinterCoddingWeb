3662869c85d815a123907b3dc3198c9155b21ea3
Girlfirend-Yerin <hsunj2003@naver.com>
2019-10-30 12:38:45 +0900

    Fix: Attach Jquery Operation in Modal
    Modal Dialog가 열리는 Operation 추가
    누락된 사항
    
     #1

---
+++ public/javascripts/index.js
@@ -0,0 +1,36 @@
+import _fetch from '../utils/fetchUtil.js'
+
+$('.card-lecture').click(function () {
+    $('#modal-lecture-info').modal('show');
+});
+  
+$('.lecture-time > a').click(function () {
+    $('#modal-lecture-task').modal('show');
+});
+  
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+  
+$(function () {
+    $('[data-toggle="popover"]').popover({
+      container: 'body',
+      html: true,
+      placement: 'right',
+      sanitize: false,
+      content: function () {
+      return $("#PopoverContent").html();
+      }
+    });
+  });
+
+$('.form-search > form-control').keyup(async event => {
+    const value = event.target.value
+
+    const url = _fetch.fetchAwait
+})
+
+
+window.onload = () => {
+
+}
