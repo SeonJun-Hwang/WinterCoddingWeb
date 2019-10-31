3d3b14dac7559c8e23d5e98b680561f6b76d0400
Girlfirend-Yerin <hsunj2003@naver.com>
2019-10-31 11:43:04 +0900

    Feat : Dynamic render of lecture
    강의 정보를 출력해주는 기능 추가
    추가 가능 여부에 맞게 출력
    
    close #9

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,31 @@
-import _fetch from '../utils/fetchUtil.js'
+import renders from './renders.js'
+import lectures from './models/lectures.js'
+import timetable from './models/timetable.js'
 
-$('.card-lecture').click(function () {
+$('.list-lecture').click(event => {
+    let target = event.target
+    if (target.classList.contains('list-lecture')) return;
+
+    while (!target.classList.contains('card-lecture'))
+        target = target.parentElement
+
+    const idx = $('.list-lecture').children().index(target)
+    lectures.setOpenLecture(idx)
+    renders.lectureInfo(lectures.getDataOfIdx(idx))
+
     $('#modal-lecture-info').modal('show');
 });
   
-$('.lecture-time > a').click(function () {
-    $('#modal-lecture-task').modal('show');
-});
+// $('.timeline-vertical > ul').click(event => {
+//     let target = $(event.target)
+//     if (target.get(0).tagName == 'UL') return;
+//     target = target.closest('.lecture-time')
+//     const dataEvent = target.data('event')
+//     const idx = dataEvent.replace(/[^0-9]/g,'')
+//     renders.scheduleInfo(timetable.getSchedule(idx - 1))
+
+//     $('#modal-lecture-task').modal('show');
+// });
   
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
@@ -24,13 +43,23 @@ $(function () {
     });
   });
 
-$('.form-search > form-control').keyup(async event => {
+$('#search-form > .form-control').keyup(async event => {
     const value = event.target.value
-
-    const url = _fetch.fetchAwait
+    const searchResult = await lectures.searchLecture(value)
+    $('.list-lecture').html(renders.searchList(searchResult))
 })
 
+$('#regist-lecture').click(event => {
+    const number = timetable.addSchedule(lectures.getOpenLecture())
+    renders.addSchedule(lectures.getOpenLecture(), number)
+
+    $('#modal-lecture-info').modal('hide');
+})
 
 window.onload = () => {
-
+    const today = new Date().getDay()
+    if (0 < today && today < 6){
+        $(`.list-lecture-item`).children(`:eq(${today - 1})`).children(`:first`).addClass('today')
+    }
+    
 }
