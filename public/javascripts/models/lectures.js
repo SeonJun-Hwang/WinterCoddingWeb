eba49a1d778bd69c7962b4523b40943ae85e6257
Girlfirend-Yerin <hsunj2003@naver.com>
2019-10-31 11:38:16 +0900

    Feat: Create Lecture Model
    강의 목록을 출력하기 위한 Model 추가
    API값을 가져와서 출력
    
    close #7

---
+++ public/javascripts/models/lectures.js
@@ -0,0 +1,30 @@
+import _fetch from '../../utils/fetchUtil.js'
+
+const lecture = {
+    async searchLecture(query) {
+        this.lecture = []
+
+        if (query.length >= 2) {
+            const result = await _fetch.fetchAwait(`/api/${query}`, _fetch.option.get)
+
+            if (Array.isArray(result)) this.lecture = result
+        }
+
+        return this.lecture
+    },
+    getDataOfIdx(idx){
+        return this.lecture[idx]
+    },
+    setOpenLecture(idx){
+        this._openLecture = this.lecture[idx];
+    },
+    getOpenLecture(){
+        return this._openLecture
+    },
+    resetOpenLecture(){
+        this._openLecture = undefined;
+    }
+
+}
+
+export default lecture
